9bb2f3066ab3024ebfddf7800de2285bde1fad09
Chetan Urkude <chetan.urkude@helpshift.com>
2024-10-26 13:20:14 +0530

    Add basic and common handler run-jetty function in core/-main

--- src/noob_project/core.clj
+++ src/noob_project/core.clj
@@ -1,8 +1,17 @@
 (ns noob-project.core
-  (:gen-class))
+  (:gen-class)
+   (:require [ring.adapter.jetty :as jetty]
+             [clojure.pprint]))
 
-;; (defn -main
-;;   "I don't do a whole lot ... yet."
-;;   [& args]
-;;   (println "Hello, World!"))
+(defn handler [request]
+  (clojure.pprint/pprint request)
+  {:status 200
+   :headers {"Content-Type" "text/html" "Custom Header" "Cusotm Header Value" "Another Header Key1 " "Another Header key2"}
+   :body "Welcome to Noob Project 2.0"})
 
+(defn -main
+  "This is -main function of core.clj"
+  [& args]
+  (jetty/run-jetty handler
+                   {:port 3000
+                    :join? true}))
