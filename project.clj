fb65b83bd2bf60056235bec12b1f2129f2a73ba8
Stormbreaker <chetanurkude111@gmail.com>
2024-10-31 20:00:19 +0530

    Add dependency for monger

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.novemberain/monger "3.5.0"]
                  [ring/ring-core "1.9.5"]
                  [ring/ring-jetty-adapter "1.9.5"]
                  [compojure "1.6.2"]]
