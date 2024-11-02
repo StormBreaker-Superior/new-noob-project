633f2ffb01a3457513b800d6f2c2cbcd64e0d344
Stormbreaker <chetanurkude111@gmail.com>
2024-11-02 23:32:57 +0530

    Fail task creation if section doesn't exist

--- src/noob_project/handlers.clj
+++ src/noob_project/handlers.clj
@@ -65,28 +65,32 @@
     (str "Showing description for " taskId " in section " sectionId)))
 
 (defn create-task [request]
-  (try (let [body (:params request)
-             sectionName (:sectionId body)
-             taskName (:taskName body)
-             taskDescription (:taskDescription body)
-             collection "tasks"
-             data {:sectionName sectionName :taskName taskName :taskDescription taskDescription}]
+  (try
+    (let [body (:params request)
+          sectionName (:sectionId body)
+          taskName (:taskName body)
+          taskDescription (:taskDescription body)
+          collection "tasks"
+          data {:sectionName sectionName :taskName taskName :taskDescription taskDescription}]
 
-         (pprint/pprint (str "body" body "\n" "Task creation requested for" data))
-         (if
-          (and (not-empty sectionName) (not-empty taskName))
+      (pprint/pprint (str "body" body "\n" "Task creation requested for" data))
+      (if
+       (and (not-empty sectionName) (not-empty taskName))
           ;; TODO: if section doesn't exist, then what should be the behaviour
            ;; (1) fail the request 
            ;; (2) create section
-           (let [insertResult (db/insert-data collection data)]
-             (if insertResult
-               ;; TODO: allow only string value in taskName
-               (-> (response/response "Task Added Successfully")
-                   (response/status 201))
-               (-> (response/response "Internal Server Error.Db insertion failed")
-                   (response/status 501))))
-           (-> (response/response "Bad Request. No sectionId or taskName passed")
-               (response/status 400))))
-       (catch Exception e
-         (-> (response/response "Internal Server Error")
-             (response/status 500)))))
+        (if (db/data-exists? "sections" {:sectionName sectionName})
+          (let [insertResult (db/insert-data collection data)]
+            (if insertResult
+                  ;; TODO: allow only string value in taskName
+              (-> (response/response "Task Added Successfully")
+                  (response/status 201))
+              (-> (response/response "Internal Server Error.Db insertion failed")
+                  (response/status 501))))
+          (-> (response/response (str "section \"" sectionName "\" doesn't exist"))
+              (response/status 404)))
+        (-> (response/response "Bad Request. No sectionId or taskName passed")
+            (response/status 400))))
+    (catch Exception e
+      (-> (response/response "Internal Server Error")
+          (response/status 500)))))
