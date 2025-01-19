21dec810b23da59dd5472fae9c8c7b2a5d350e50
Stormbreaker <chetanurkude111@gmail.com>
2025-01-19 12:45:55 +0530

    Use context in routes.clj

--- src/noob_project/routes.clj
+++ src/noob_project/routes.clj
@@ -5,11 +5,13 @@
 
 (compojure/defroutes app
   (compojure/GET "/" params handlers/home)
-  (compojure/GET "/:sectionId" params handlers/get-tasks)
-  (compojure/GET "/:sectionId/:taskId" params handlers/task)
-  (compojure/POST "/sections" params handlers/create-section)
-  (compojure/POST "/:sectionId" params handlers/create-task)
-  (compojure/PATCH "/:sectionId/tasks/:taskId" params handlers/update-task)
-  (compojure/DELETE "/:sectionId/tasks/:taskId" params handlers/delete-task)
-  (compojure/DELETE "/sections/:sectionId" params handlers/delete-section)
+  (compojure/context "/tasks" []
+    (compojure/GET "/:taskId" params handlers/task)
+    (compojure/POST "/" params handlers/create-task)
+    (compojure/PATCH "/:taskId" params handlers/update-task))
+  (compojure/context "/sections" []
+    (compojure/GET "/:sectionId" params handlers/get-tasks)
+    (compojure/POST "/" params handlers/create-section)
+    (compojure/DELETE ":sectionId" params handlers/delete-section))
+  ;; (compojure/DELETE "/:sectionId/tasks/:taskId" params handlers/delete-task)
   (route/not-found "Page Not Found !"))
